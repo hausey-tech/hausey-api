65666264500d1ae21b33c4222c5ff904bb40e151
Bruno Dias <bruno_soares_dias@hotmail.com>
2022-11-17 00:30:18 -0300

    docs: change description

--- src/shared/docs/index.ts
+++ src/shared/docs/index.ts
@@ -22,7 +22,7 @@ export default {
   openapi: '3.0.0',
   info: {
     title: 'Hausey API',
-    description: 'Hausey node.js API rest',
+    description: 'Node.js API REST',
     version: '1.0.0',
   },
   license: {
