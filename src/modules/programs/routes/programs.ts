28af3ec66966f36a33de956a5532288e6115d3cb
Bruno Dias <bruno_soares_dias@hotmail.com>
2022-11-18 17:47:22 -0300

    feat: ensure auth on index programs

--- src/modules/programs/routes/programs.ts
+++ src/modules/programs/routes/programs.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 
+import { ensureAuthentication } from '../../users/middlewares/ensure-authentication';
 import { ProgramsController } from '../controllers/programs';
 
 export const programsRouter = Router();
 const programsController = new ProgramsController();
 
-programsRouter.get('/', programsController.index);
+programsRouter.get('/', ensureAuthentication, programsController.index);
