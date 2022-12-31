86903ae012b2f2dd16c9beaf22ccc7d0bc765d98
Bruno Dias <bruno_soares_dias@hotmail.com>
2022-12-31 03:17:28 -0300

    docs: list patient anamneses

--- src/shared/docs/paths/patients.ts
+++ src/shared/docs/paths/patients.ts
@@ -37,6 +37,53 @@ export const patientsPath = {
     },
   },
   anamneses: {
+    get: {
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+      tags: ['Patients'],
+      summary: 'Shows all patient anamneses',
+      parameters: [
+        {
+          in: 'path',
+          name: 'patientId',
+          schema: {
+            type: 'string',
+          },
+          required: false,
+          description: 'Patient id',
+        },
+      ],
+      responses: {
+        200: {
+          description: 'Success',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'array',
+                items: {
+                  $ref: '#/schemas/patientAnamnesis',
+                },
+              },
+            },
+          },
+          400: {
+            $ref: '#/components/badRequest',
+          },
+          401: {
+            $ref: '#/components/unauthorized',
+          },
+          404: {
+            $ref: '#/components/notFound',
+          },
+          500: {
+            $ref: '#/components/serverError',
+          },
+        },
+      },
+    },
     post: {
       security: [
         {
