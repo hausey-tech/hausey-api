32a3bedff3f6447395c745739e5a04cb3ca71b82
Bruno Dias <bruno_soares_dias@hotmail.com>
2024-02-02 16:20:08 -0300

    fix: survey monkey head request

--- src/modules/integrations/routes/survey-monkey.routes.ts
+++ src/modules/integrations/routes/survey-monkey.routes.ts
@@ -4,6 +4,10 @@ import { SurveyMonkeyController } from '../controllers/survey-monkey-controller'
 export const surveyMonkeyRouter = Router();
 const surveyMonkeyController = new SurveyMonkeyController();
 
+surveyMonkeyRouter.get('/survey-monkey/webhook', (req, res) => {
+  res.send();
+});
+
 surveyMonkeyRouter.post(
   '/survey-monkey/webhook',
   surveyMonkeyController.webhook,
