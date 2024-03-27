89352fa5a8031010f34c974bd1ef5cba9634c285
Jonathan Richard <jonathanrichard947@gmail.com>
2024-03-27 12:49:58 -0300

    feat: changed coupon to production stripe

--- src/modules/integrations/services/stripe/create-coupon.ts
+++ src/modules/integrations/services/stripe/create-coupon.ts
@@ -14,7 +14,7 @@ export class CreateCoupon {
   }: Props): Promise<Stripe.Response<Stripe.PromotionCode>> {
     try {
       const coupon = await stripeInstance.promotionCodes.create({
-        coupon: 'lUkNd6Mb',
+        coupon: 'FWIJTrrH',
         code,
       });
       return coupon;
