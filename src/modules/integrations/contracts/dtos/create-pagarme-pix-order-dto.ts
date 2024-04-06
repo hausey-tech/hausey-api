50475614022953a1f8b4cce30282f30c6d2232e8
Bruno Dias <bruno_soares_dias@hotmail.com>
2024-04-06 14:57:54 -0300

    fix: pix bug

--- src/modules/integrations/contracts/dtos/create-pagarme-pix-order-dto.ts
+++ src/modules/integrations/contracts/dtos/create-pagarme-pix-order-dto.ts
@@ -1,9 +1,17 @@
 import { Plan } from '../../../plans/entities/plan';
-import { IPagarmeSplitDTO } from './pagarme/pagarme-split-dto';
 
 export interface ICreatePagarmePixOrderDTO {
   customerId: string;
   plan: Plan;
   price: number;
-  split?: IPagarmeSplitDTO[];
+  split?: {
+    amount: number;
+    recipientId: string;
+    type: string;
+    options: {
+      chargeProcessingFee: boolean;
+      chargeRemainderFee: boolean;
+      liable: boolean;
+    };
+  }[];
 }
--- src/modules/integrations/services/pagarme/create-pagarme-pix-order-service.ts
+++ src/modules/integrations/services/pagarme/create-pagarme-pix-order-service.ts
@@ -2,7 +2,6 @@ import { injectable } from 'tsyringe';
 import { AppError } from '../../../../shared/errors/app-error';
 import { ICreatePagarmePixOrderDTO } from '../../contracts/dtos/create-pagarme-pix-order-dto';
 import { pagarmeInstance } from '../../utils/pagarme-instance';
-import { IPagarmeChargeDTO } from '../../contracts/dtos/pagarme/pagarme-charge-dto';
 
 export interface IPix {
   qrCode: string;
@@ -18,38 +17,48 @@ export class CreatePagarmePixOrderService {
     split,
   }: ICreatePagarmePixOrderDTO): Promise<IPix> {
     try {
-      const { data }: { data: IPagarmeChargeDTO } = await pagarmeInstance.post(
-        '/orders',
-        {
-          customer_id: customerId,
-          items: [
-            {
-              amount: price,
-              description: `Plano ${plan.id}`,
-              quantity: 1,
-              code: plan.id,
+      const { data } = await pagarmeInstance.post('/orders', {
+        customer_id: customerId,
+        items: [
+          {
+            amount: price,
+            description: `Plano ${plan.id}`,
+            quantity: 1,
+            code: plan.id,
+          },
+        ],
+        payments: [
+          {
+            split:
+              split?.length > 0 &&
+              !(process.env.PAGARME_SECRET_KEY.split('_')[1] === 'test')
+                ? split.map(sp => ({
+                    amount: sp.amount,
+                    recipient_id: sp.recipientId,
+                    type: sp.type,
+                    options: {
+                      charge_processing_fee: sp.options.chargeProcessingFee,
+                      charge_remainder_fee: sp.options.chargeRemainderFee,
+                      liable: sp.options.liable,
+                    },
+                  }))
+                : undefined,
+            payment_method: 'pix',
+            pix: {
+              expires_in: '300',
+              additional_information: [
+                {
+                  name: `Plano`,
+                  value: plan.name,
+                },
+              ],
             },
-          ],
-          payments: [
-            {
-              split,
-              payment_method: 'pix',
-              pix: {
-                expires_in: '300',
-                additional_information: [
-                  {
-                    name: `Plano`,
-                    value: plan.name,
-                  },
-                ],
-              },
-            },
-          ],
-        },
-      );
+          },
+        ],
+      });
       return {
-        qrCode: data.last_transaction.qr_code,
-        expiresAt: data.last_transaction.expires_at,
+        qrCode: data.charges[0].last_transaction?.qr_code,
+        expiresAt: data.charges[0].last_transaction?.expires_at,
       };
     } catch (error) {
       console.error(error.response.data);
