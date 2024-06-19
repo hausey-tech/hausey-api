723419a88074b2c6e20cb30991ed2a3dbae39371
Bruno Dias <bruno_soares_dias@hotmail.com>
2024-06-19 16:54:53 -0300

    chore: add empty on create address schema

--- src/modules/addresses/contracts/schemas/CreateAddressSchema.ts
+++ src/modules/addresses/contracts/schemas/CreateAddressSchema.ts
@@ -4,10 +4,10 @@ export const CreateAddressSchema = {
   [Segments.BODY]: Joi.object().keys({
     patientId: Joi.string().uuid().required(),
     addressLine1: Joi.string().max(50).required(),
-    addressLine2: Joi.string().max(50),
+    addressLine2: Joi.string().max(50).empty(''),
     city: Joi.string().required(),
     state: Joi.string().required(),
-    zipOrPostcode: Joi.string(),
+    zipOrPostcode: Joi.string().empty(''),
     country: Joi.string().required(),
   }),
 };
