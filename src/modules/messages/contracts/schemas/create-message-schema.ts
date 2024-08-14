8ad57adaa823df140f37a360ef4e761402aed1fc
Bruno Dias <bruno_soares_dias@hotmail.com>
2024-08-14 20:12:33 -0300

    chore: add suporte to multiple ids on create message endpoint

--- src/modules/messages/contracts/schemas/create-message-schema.ts
+++ src/modules/messages/contracts/schemas/create-message-schema.ts
@@ -3,7 +3,10 @@ import { Joi, Segments } from 'celebrate';
 export const CreateMessageSchema = {
   [Segments.BODY]: Joi.object().keys({
     type: Joi.string().valid('inApp', 'push').required(),
-    to: Joi.string().required(),
+    to: Joi.alternatives(
+      Joi.string(),
+      Joi.array().items(Joi.string()),
+    ).required(),
     title: Joi.string(),
     body: Joi.string(),
     link: Joi.string(),
--- src/modules/messages/services/create-message-to-user-service.ts
+++ src/modules/messages/services/create-message-to-user-service.ts
@@ -6,6 +6,10 @@ import { type IMessagesRepository } from '../contracts/repositories/messages-rep
 import { IPatientsRepository } from '../../patients/contracts/repositories/patients';
 import { AppError } from '../../../shared/errors/app-error';
 
+interface IProps extends Omit<ICreateMessageDTO, 'to'> {
+  to: string[] | string;
+}
+
 @injectable()
 export class CreateMessageToUserService {
   constructor(
@@ -16,38 +20,65 @@ export class CreateMessageToUserService {
     private readonly patientsRepository: IPatientsRepository,
   ) {}
 
-  public async execute({
-    type,
-    to,
-    title,
-    body,
-    link,
-  }: ICreateMessageDTO): Promise<void> {
-    const user = await this.patientsRepository.findById(to);
-    if (!user) {
-      throw new AppError(
-        'Paciente não encontrado, verifique e tente novamente!',
+  public async execute({ type, to, title, body, link }: IProps): Promise<void> {
+    if (Array.isArray(to)) {
+      await Promise.all(
+        to.map(async userId => {
+          const user = await this.patientsRepository.findById(userId);
+          if (user) {
+            if (user.fcmToken) {
+              try {
+                const sendPushService = container.resolve(
+                  SendFirebaseMessagingService,
+                );
+                await sendPushService.execute({
+                  token: user.fcmToken as string,
+                  notification: { title, body },
+                  data: { link },
+                });
+              } catch {
+                console.log('Erro ao enviar FCM (265)');
+              }
+            }
+            await this.messagesRepository.create({
+              type,
+              to: userId,
+              title,
+              body,
+              link,
+            });
+          }
+        }),
       );
-    }
-    if (user.fcmToken) {
-      try {
-        const sendPushService = container.resolve(SendFirebaseMessagingService);
-        await sendPushService.execute({
-          token: user.fcmToken as string,
-          notification: { title, body },
-          data: { link },
-        });
-      } catch {
-        console.log('Erro ao enviar FCM');
+    } else {
+      const user = await this.patientsRepository.findById(to);
+      if (!user) {
+        throw new AppError(
+          'Paciente não encontrado, verifique e tente novamente!',
+        );
       }
-    }
+      if (user.fcmToken) {
+        try {
+          const sendPushService = container.resolve(
+            SendFirebaseMessagingService,
+          );
+          await sendPushService.execute({
+            token: user.fcmToken as string,
+            notification: { title, body },
+            data: { link },
+          });
+        } catch {
+          console.log('Erro ao enviar FCM');
+        }
+      }
 
-    await this.messagesRepository.create({
-      type,
-      to,
-      title,
-      body,
-      link,
-    });
+      await this.messagesRepository.create({
+        type,
+        to,
+        title,
+        body,
+        link,
+      });
+    }
   }
 }
