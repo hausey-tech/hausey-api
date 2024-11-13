333dfcdfd0e3af3552a939c833831ec22c4fe13b
Bruno Bastos <brunobastos@edu.unifor.br>
2024-11-13 11:33:57 -0300

    feat: alterando a injeção de dependencia

--- src/modules/alert-professional/services/alertProfessional.service.ts
+++ src/modules/alert-professional/services/alertProfessional.service.ts
@@ -1,15 +1,12 @@
-import { inject, injectable } from 'tsyringe';
+import { container, injectable } from 'tsyringe';
 import { CreateCallService } from '../../integrations/services/programmable-voice-twilio';
 
 @injectable()
 export class AlertProfessionalService {
-  constructor(
-    @inject('CreateCallService')
-    private service: CreateCallService,
-  ) {}
-
   public async execute(): Promise<any> {
     // const tries = 1;
-    await this.service.createCall();
+    const callService = container.resolve(CreateCallService);
+
+    await callService.createCall();
   }
 }
