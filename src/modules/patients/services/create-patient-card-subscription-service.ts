68fe061f02cd93b68bf01a786c204aa8f5237338
Hausey Development <hauseydevs@gmail.com>
2024-12-04 21:44:31 -0300

    feat: add loggins info

--- src/modules/patients/services/create-patient-card-subscription-service.ts
+++ src/modules/patients/services/create-patient-card-subscription-service.ts
@@ -1,5 +1,6 @@
 import { injectable, inject, container } from 'tsyringe';
 import { isBefore } from 'date-fns';
+import { Logger } from 'pino';
 import { CreatePagarmeSubscriptionService } from '../../integrations/services/pagarme/create-pagarme-subscription-service';
 import { ISellerCodesRepository } from '../../seller-codes/contracts/repositories/seller-codes';
 import { AppError } from '../../../shared/errors/app-error';
@@ -35,6 +36,9 @@ export class CreatePatientCardSubscriptionService {
 
     @inject('PlansRepository')
     private plansRepository: IPlansRepository,
+
+    @inject('Logger')
+    private logger: Logger,
   ) {}
 
   public async execute({
@@ -44,134 +48,152 @@ export class CreatePatientCardSubscriptionService {
     cardToken,
     address,
   }: IProps): Promise<void> {
-    const patient = await this.patientsRepository.findById(patientId);
-    if (!patient) {
-      throw new AppError(
-        'Paciente não encontrado, verifique e tente novamente!',
-      );
-    }
-    if (!patient.stripeCustomerId) {
-      throw new AppError(
-        'Paciente não possui conta de pagamento, entre em contato com o suporte!',
-      );
-    }
-    if (patient.planExpiresAt && isBefore(new Date(), patient.planExpiresAt)) {
-      throw new AppError('Paciente já possui assinatura vigente!');
-    }
-    const plan = await this.plansRepository.findyByPriceId(planId);
-    if (!plan.stripePriceId) {
-      throw new AppError(
-        'Plano não encontrado, entre em contato com o suporte!',
-      );
-    }
-    const split = [];
-    const discounts = [];
-    if (patient.sellerId) {
-      const sellerCode = await this.sellerCodesRepository.findBySellerId(
-        patient.sellerId,
-      );
-      const sellerCodeDiscount = sellerCode.discounts.find(
-        d => d.planId === plan.id,
-      );
-      if (sellerCodeDiscount) {
-        discounts.push({
-          discountType: 'flat',
-          value: sellerCodeDiscount.discount,
-        });
+    try {
+      const patient = await this.patientsRepository.findById(patientId);
+      if (!patient) {
+        throw new AppError(
+          'Paciente não encontrado, verifique e tente novamente!',
+        );
       }
-      let sellersPart = 0;
-      sellerCode.sellers.forEach(sellerCodeSeller => {
-        if (!sellerCodeSeller.seller.recipientId) {
-          throw new AppError(
-            'Há um problema com seu cupom, entre em contato com o suporte!',
-          );
+      if (!patient.stripeCustomerId) {
+        throw new AppError(
+          'Paciente não possui conta de pagamento, entre em contato com o suporte!',
+        );
+      }
+      if (
+        patient.planExpiresAt &&
+        isBefore(new Date(), patient.planExpiresAt)
+      ) {
+        throw new AppError('Paciente já possui assinatura vigente!');
+      }
+      const plan = await this.plansRepository.findyByPriceId(planId);
+      if (!plan.stripePriceId) {
+        throw new AppError(
+          'Plano não encontrado, entre em contato com o suporte!',
+        );
+      }
+      const split = [];
+      const discounts = [];
+      if (patient.sellerId) {
+        const sellerCode = await this.sellerCodesRepository.findBySellerId(
+          patient.sellerId,
+        );
+        const sellerCodeDiscount = sellerCode.discounts.find(
+          d => d.planId === plan.id,
+        );
+        if (sellerCodeDiscount) {
+          discounts.push({
+            discountType: 'flat',
+            value: sellerCodeDiscount.discount,
+          });
         }
-        sellersPart += sellerCodeSeller.fee;
-        split.push({
-          amount: sellerCodeSeller.fee,
-          recipientId: sellerCodeSeller.seller.recipientId,
-          type: 'percentage',
-          options: {
-            chargeProcessingFee: false,
-            chargeRemainderFee: false,
-            liable: false,
-          },
+        let sellersPart = 0;
+        sellerCode.sellers.forEach(sellerCodeSeller => {
+          if (!sellerCodeSeller.seller.recipientId) {
+            throw new AppError(
+              'Há um problema com seu cupom, entre em contato com o suporte!',
+            );
+          }
+          sellersPart += sellerCodeSeller.fee;
+          split.push({
+            amount: sellerCodeSeller.fee,
+            recipientId: sellerCodeSeller.seller.recipientId,
+            type: 'percentage',
+            options: {
+              chargeProcessingFee: false,
+              chargeRemainderFee: false,
+              liable: false,
+            },
+          });
         });
-      });
 
-      if (sellerCode.fee && sellerCode.fee > 0) {
-        if (!sellerCode.seller.recipientId) {
-          throw new AppError(
-            'Há um problema com seu código de desconto, entre em contato com o suporte!',
-          );
+        if (sellerCode.fee && sellerCode.fee > 0) {
+          if (!sellerCode.seller.recipientId) {
+            throw new AppError(
+              'Há um problema com seu código de desconto, entre em contato com o suporte!',
+            );
+          }
+          sellersPart += sellerCode.fee;
+          split.push({
+            amount: sellerCode.fee,
+            recipientId: sellerCode.seller.recipientId,
+            type: 'percentage',
+            options: {
+              chargeProcessingFee: false,
+              chargeRemainderFee: false,
+              liable: false,
+            },
+          });
         }
-        sellersPart += sellerCode.fee;
-        split.push({
-          amount: sellerCode.fee,
-          recipientId: sellerCode.seller.recipientId,
-          type: 'percentage',
-          options: {
-            chargeProcessingFee: false,
-            chargeRemainderFee: false,
-            liable: false,
-          },
-        });
+
+        if (sellersPart > 0) {
+          split.push({
+            amount: 100 - sellersPart,
+            recipientId: process.env.PAGARME_RECIPIENT_ID,
+            type: 'percentage',
+            options: {
+              chargeProcessingFee: true,
+              chargeRemainderFee: true,
+              liable: true,
+            },
+          });
+        }
       }
 
-      if (sellersPart > 0) {
-        split.push({
-          amount: 100 - sellersPart,
-          recipientId: process.env.PAGARME_RECIPIENT_ID,
-          type: 'percentage',
-          options: {
-            chargeProcessingFee: true,
-            chargeRemainderFee: true,
-            liable: true,
-          },
+      const cancelPagarmeCustomerSubscriptionsService = container.resolve(
+        CancelPagarmeCustomerSubscriptionsService,
+      );
+
+      await cancelPagarmeCustomerSubscriptionsService.execute({
+        customerId: patient.stripeCustomerId,
+      });
+
+      const createPagarmeSubscriptionService = container.resolve(
+        CreatePagarmeSubscriptionService,
+      );
+
+      if (
+        patient.stripeCustomerId === null ||
+        patient.stripeCustomerId === undefined
+      ) {
+        this.logger.info({
+          message: 'Logger dentro dos 6 meses.',
+        });
+        await createPagarmeSubscriptionService.execute({
+          planId,
+          paymentMethod,
+          cardToken,
+          customerId: patient.stripeCustomerId,
+          split,
+          discounts,
+          address,
+          intervalCount: 6,
+        });
+      } else {
+        this.logger.info({
+          message: 'Logger dentro de 1 mês.',
+        });
+        await createPagarmeSubscriptionService.execute({
+          planId,
+          paymentMethod,
+          cardToken,
+          customerId: patient.stripeCustomerId,
+          split,
+          discounts,
+          address,
+          intervalCount: 1,
         });
       }
-    }
-
-    const cancelPagarmeCustomerSubscriptionsService = container.resolve(
-      CancelPagarmeCustomerSubscriptionsService,
-    );
-
-    await cancelPagarmeCustomerSubscriptionsService.execute({
-      customerId: patient.stripeCustomerId,
-    });
-
-    const createPagarmeSubscriptionService = container.resolve(
-      CreatePagarmeSubscriptionService,
-    );
-
-    if (
-      patient.stripeCustomerId === null ||
-      patient.stripeCustomerId === undefined
-    ) {
-      await createPagarmeSubscriptionService.execute({
-        planId,
-        paymentMethod,
-        cardToken,
-        customerId: patient.stripeCustomerId,
-        split,
-        discounts,
-        address,
-        intervalCount: 6,
+      await this.patientsRepository.update(patient.id, {
+        planId: plan.id,
       });
-    } else {
-      await createPagarmeSubscriptionService.execute({
-        planId,
-        paymentMethod,
-        cardToken,
-        customerId: patient.stripeCustomerId,
-        split,
-        discounts,
-        address,
-        intervalCount: 1,
-      });
+    } catch (error) {
+      this.logger.info(
+        {
+          error,
+        },
+        'Houve um erro ao efetuar o pagamento.',
+      );
     }
-    await this.patientsRepository.update(patient.id, {
-      planId: plan.id,
-    });
   }
 }
