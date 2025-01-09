dbd9ac025f2e04ae98b97652111bb1a3e04afdeb
Bruno Bastos <brunobastos@edu.unifor.br>
2025-01-09 14:36:48 -0300

    feat: fix construtor

--- src/modules/plans/controllers/plans.ts
+++ src/modules/plans/controllers/plans.ts
@@ -1,23 +1,12 @@
 import { Request, Response } from 'express';
-import { container, inject } from 'tsyringe';
-import { Logger } from 'pino';
+import { container } from 'tsyringe';
 import { FindPlans } from '../services/find-plans';
 import { CreatePlanService } from '../services/create-plan-service';
 
 export class PlansController {
-  constructor(
-    @inject('Logger')
-    private logger: Logger,
-  ) {}
-
   public async index(request: Request, response: Response): Promise<Response> {
     const { regions } = request.query;
-    this.logger.info(
-      {
-        region: regions,
-      },
-      'Região informada pelo usuário',
-    );
+    console.log('Region informada pelo usuário', regions);
     const findPlansService = container.resolve(FindPlans);
     const plans = await findPlansService.execute({
       regions: regions as string,
