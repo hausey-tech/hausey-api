7fc7a1e88fa3f7f709aedc660a0b4fb53131b922
Bruno Bastos <brunobbastosf@gmail.com>
2025-01-27 10:29:45 -0300

    feat: finalizando

--- src/modules/patients/services/filter-admin.ts
+++ src/modules/patients/services/filter-admin.ts
@@ -2,7 +2,6 @@ import { injectable, inject } from 'tsyringe';
 
 import { Logger } from 'pino';
 import { AppError } from '../../../shared/errors/app-error';
-import { ISellerCodeSellersRepository } from '../../seller-code-sellers/contracts/repositories/seller-code-sellers-repository';
 import { ISellerCodesRepository } from '../../seller-codes/contracts/repositories/seller-codes';
 import { IPatientsRepository } from '../contracts/repositories/patients';
 import { Patient } from '../entities/patient';
@@ -35,9 +34,6 @@ export class FilterAdminService {
     @inject('PatientsRepository')
     private patientsRepository: IPatientsRepository,
 
-    @inject('SellerCodeSellersRepository')
-    private sellerCodeSellersRepository: ISellerCodeSellersRepository,
-
     @inject('SellerCodesRepository')
     private sellerCodesRepository: ISellerCodesRepository,
 
@@ -56,175 +52,76 @@ export class FilterAdminService {
   }): Promise<
     SellerCodeResponse | PatientsPaginatedResponse | Patient | Patient[] | any
   > {
-    const { userId, type, page = 1, limit = 10 } = query;
+    try {
+      const { userId, page = 1, limit = 10 } = query;
 
-    if (Number.isNaN(page) || Number(page) < 1) {
-      throw new AppError('Page must be a valid number');
-    }
-
-    if (Number.isNaN(limit) || Number(limit) < 1) {
-      throw new AppError('Invalid limit value');
-    }
-
-    const skip = (Number(page) - 1) * Number(limit);
-    const take = Number(limit);
-
-    if (userId) {
-      const user = await this.usersRepository.findById(userId);
-
-      if (user.role.name !== 'Administrador') {
-        throw new AppError('Você não tem permissão suficiente', 401);
+      if (Number.isNaN(page) || Number(page) < 1) {
+        throw new AppError('Page must be a valid number');
       }
-      const sellerCodeSellers =
-        await this.sellerCodeSellersRepository.findAll();
 
-      const validSellerCodeSellers = sellerCodeSellers.filter(
-        sellerCodeSeller =>
-          sellerCodeSeller.sellerCodeId && sellerCodeSeller.sellerId,
-      );
+      if (Number.isNaN(limit) || Number(limit) < 1) {
+        throw new AppError('Invalid limit value');
+      }
 
-      const allPatients = await this.patientsRepository.findAll();
+      // const skip = (Number(page) - 1) * Number(limit);
+      // const take = Number(limit);
 
-      const patientsWhereSellerIdIsNull: Patient[] = allPatients.filter(
-        patient => !patient.sellerId,
-      );
+      if (userId) {
+        const user = await this.usersRepository.findById(userId);
 
-      const totalPages = Math.ceil(patientsWhereSellerIdIsNull.length / take);
+        if (!user) {
+          throw new AppError('Usuário não encontrado.', 404);
+        }
 
-      const sellerCodes = await Promise.all(
-        validSellerCodeSellers.map(async sellerCodeSeller => {
-          try {
-            const sellerCode =
-              type !== undefined
-                ? await this.sellerCodesRepository.findByIdAndType(
-                    sellerCodeSeller.sellerCodeId,
-                    type,
-                  )
-                : await this.sellerCodesRepository.findById(
-                    sellerCodeSeller.sellerCodeId,
-                  );
+        if (user.role.name !== 'Administrador') {
+          throw new AppError('Você não tem permissão suficiente', 401);
+        }
 
-            if (!sellerCode) {
-              this.logger.warn(
-                `Seller not found for sellerCode ID: ${sellerCodeSeller.sellerCodeId}`,
-              );
-              return null;
-            }
+        const sellerCodes = await this.sellerCodesRepository.findAll();
 
-            if (!sellerCodeSeller.sellerId) {
-              this.logger.warn(
-                `Seller not found for SellerCodeSeller ID: ${sellerCodeSeller.sellerId}`,
-              );
-              return null;
-            }
-
-            const [patients, totalPatients] =
-              await this.patientsRepository.findBySellerIdPaginated(
-                sellerCode.sellerId,
-                skip,
-                take,
-              );
-
-            const totalPagesAllPatients = Math.ceil(totalPatients / take);
-
-            const paginatedPatients: PatientsPaginatedResponse = {
-              patients,
-              totalPatients,
-              totalPages: totalPagesAllPatients,
-            };
-
-            const userInfo = await this.usersRepository.findById(
+        const sellerCodeMapper = await Promise.all(
+          sellerCodes.map(async sellerCode => {
+            const patients = await this.patientsRepository.findAllBySellerId(
               sellerCode.sellerId,
             );
 
-            const sellerInfo = {
-              phoneNumber: userInfo.phoneNumber,
-              email: userInfo.email,
-              name: userInfo.name,
-              createdAt: userInfo.createdAt,
-              patients: paginatedPatients,
-              sellerCode,
-            };
-
-            return sellerInfo;
-          } catch (error) {
-            this.logger.error(
-              {
-                error: error.message,
-                stack: error.stack,
-              },
-              `Error processing sellerCodeSeller ${sellerCodeSeller.sellerCodeId}:`,
+            const userBySellerId = await this.usersRepository.findById(
+              sellerCode.sellerId,
             );
 
-            return null;
-          }
-        }),
-      );
+            if (!userBySellerId) {
+              return {
+                patients,
+                sellerCode: {
+                  id: null,
+                  name: null,
+                  phoneNumber: null,
+                  email: null,
+                  ...sellerCode,
+                },
+              };
+            }
 
-      const allSellerCodes = await this.sellerCodesRepository.findAll();
+            return {
+              sellerCode: {
+                id: userBySellerId?.id || null,
+                name: userBySellerId?.name || null,
+                phoneNumber: userBySellerId?.phoneNumber || null,
+                email: userBySellerId?.email || null,
+                ...sellerCode,
+              },
+              patients,
+            };
+          }),
+        );
 
-      const allSellerCodesNotInSellerCodesSellers = allSellerCodes.filter(
-        sellerCode =>
-          !validSellerCodeSellers.some(
-            sellerCodeSeller => sellerCodeSeller.sellerCodeId === sellerCode.id,
-          ),
-      );
+        return sellerCodeMapper;
+      }
 
-      const MapperAllSellerCodesNotInSellerCodesSellers = await Promise.all(
-        allSellerCodesNotInSellerCodesSellers.map(async sellerCodesMapper => {
-          const userMapper = await this.usersRepository.findById(
-            sellerCodesMapper.sellerId,
-          );
-
-          return {
-            id: sellerCodesMapper.id,
-            createdAt: sellerCodesMapper.createdAt,
-            name: userMapper?.name || null,
-            phoneNumber: userMapper?.phoneNumber || null,
-            email: userMapper?.email || null,
-            updatedAt: sellerCodesMapper.updatedAt,
-            deletedAt: sellerCodesMapper.deletedAt,
-            code: sellerCodesMapper.code,
-            sellerId: sellerCodesMapper.sellerId,
-            uses: sellerCodesMapper.uses,
-            active: sellerCodesMapper.active,
-            fee: sellerCodesMapper.fee,
-            maxUse: sellerCodesMapper.maxUse,
-            free: sellerCodesMapper.free,
-            type: sellerCodesMapper.type,
-          };
-        }),
-      );
-
-      const validSellerCodes = sellerCodes.filter(Boolean);
-
-      const endIndex = skip + take;
-
-      const patientsPaginated = patientsWhereSellerIdIsNull.slice(
-        skip,
-        endIndex,
-      );
-
-      return {
-        patients: {
-          patients: patientsPaginated,
-          totalPages,
-          totalPatients: patientsWhereSellerIdIsNull.length,
-        },
-        sellerCodeSellers: validSellerCodes,
-        sellerCodes: MapperAllSellerCodesNotInSellerCodesSellers,
-      };
+      throw new AppError('UserId inválido.');
+    } catch (error) {
+      this.logger.error(error.message, error);
+      throw error;
     }
-
-    const [patients, totalPatients] =
-      await this.patientsRepository.findPaginated(skip, take);
-
-    const totalPages = Math.ceil(totalPatients / take);
-
-    return {
-      patients,
-      totalPatients,
-      totalPages,
-    };
   }
 }
