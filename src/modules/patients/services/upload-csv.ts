02c7ef08d92f1fc0d8642bdaf83c5ef7ff5549f9
Bruno Bastos <brunobbastosf@gmail.com>
2025-02-20 09:38:08 -0300

    feat: return new Promise

--- src/modules/patients/services/upload-csv.ts
+++ src/modules/patients/services/upload-csv.ts
@@ -23,82 +23,87 @@ export class UploadPatientCsv {
   public async execute(file: any): Promise<string> {
     const results: any[] = [];
 
-    fs.createReadStream(file.path)
-      .pipe(
-        csv({
-          mapHeaders: ({ header }) => header.trim(),
-          mapValues: ({ value }) => value.trim(),
-        }),
-      )
-      .on('data', data => results.push(data))
-      .on('end', async () => {
-        try {
-          const createPatientPromises = results.map(async row => {
-            const {
-              name,
-              email,
-              password,
-              phoneNumber,
-              document,
-              birthdate,
-              sex,
-            } = row;
+    const fileSize = fs.statSync(file.path).size;
+    if (fileSize > 10 * 1024 * 1024) {
+      throw new AppError('Arquivo CSV excede o tamanho máximo permitido.');
+    }
 
-            if (
-              !name ||
-              !email ||
-              !password ||
-              !phoneNumber ||
-              !document ||
-              !birthdate ||
-              !sex
-            ) {
-              throw new AppError('Linha inválida no CSV', row);
-            }
+    return new Promise((resolve, reject) => {
+      fs.createReadStream(file.path)
+        .pipe(
+          csv({
+            mapHeaders: ({ header }) => header.trim(),
+            mapValues: ({ value }) => value.trim(),
+          }),
+        )
+        .on('data', data => results.push(data))
+        .on('end', async () => {
+          try {
+            const createPatientPromises = results.map(async row => {
+              const {
+                name,
+                email,
+                password,
+                phoneNumber,
+                document,
+                birthdate,
+                sex,
+              } = row;
 
-            const hashedPassword = await this.hashProvider.generateHash(
-              password,
+              if (
+                !name ||
+                !email ||
+                !password ||
+                !phoneNumber ||
+                !document ||
+                !birthdate ||
+                !sex
+              ) {
+                throw new AppError('Linha inválida no CSV', row);
+              }
+
+              const hashedPassword = await this.hashProvider.generateHash(
+                password,
+              );
+
+              const patientDto: ICreatePatientDTO = {
+                name,
+                email,
+                password: hashedPassword,
+                phoneNumber,
+                document,
+                birthdate,
+                sex,
+                sellerId: 'e00f4434-bc81-4569-ad30-8187668d43b6',
+                firstPayment: true,
+                planId: 'efe8d3ec-f3a2-432b-8a10-d7ef75e5adc2',
+              };
+
+              await this.patientsRepository.create(patientDto);
+            });
+
+            await Promise.all(createPatientPromises);
+
+            resolve('Paciente(s) criados com sucesso.');
+          } catch (error) {
+            this.logger.info(
+              {
+                message: error,
+              },
+              'Erro ao processar CSV:',
             );
-
-            const patientDto: ICreatePatientDTO = {
-              name,
-              email,
-              password: hashedPassword,
-              phoneNumber,
-              document,
-              birthdate,
-              sex,
-              sellerId: 'e00f4434-bc81-4569-ad30-8187668d43b6',
-              firstPayment: true,
-              planId: 'efe8d3ec-f3a2-432b-8a10-d7ef75e5adc2',
-            };
-
-            await this.patientsRepository.create(patientDto);
-          });
-
-          await Promise.all(createPatientPromises);
-
-          return 'Paciente(s) criados com sucesso.';
-        } catch (error) {
+            reject(new AppError('Erro ao processar CSV.'));
+          }
+        })
+        .on('error', error => {
           this.logger.info(
             {
               message: error,
             },
-            'Erro ao processar CSV:',
+            'Erro ao ler o arquivo CSV,',
           );
-          throw new AppError('Erro ao processar CSV.');
-        }
-      })
-      .on('error', error => {
-        this.logger.info(
-          {
-            message: error,
-          },
-          'Erro ao ler o arquivo CSV,',
-        );
-        throw new AppError('Erro ao ler o arquivo CSV:');
-      });
-
-    return 'Pacientes criados com sucesso.';
+          reject(new AppError('Erro ao ler o arquivo CSV:'));
+        });
+    });
   }
 }
