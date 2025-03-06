ef5ad7c893e29cf803c312be13628860781aae87
Bruno Bastos <brunobbastosf@gmail.com>
2025-03-06 19:12:45 -0300

    feat: adicionando o log no address

--- src/modules/appointments/services/find-appointments.ts
+++ src/modules/appointments/services/find-appointments.ts
@@ -1,93 +1,11 @@
-// CODIGO BACKUP, quando for implementar para o paciente
-
-// import { FindOptionsWhere, IsNull, Not } from 'typeorm';
-// import { injectable, inject } from 'tsyringe';
-
-// import { IAddressesRepository } from '../../addresses/contracts/repositories/IAddressesRepository';
-// import { IProfessionalsRepository } from '../../professionals/contracts/repositories/professionals';
-// import { Appointment } from '../entities/appointment';
-// import { IAppointmentsRepository } from '../contracts/repositories/appointments';
-// import { verifyTimeZone } from '../utils/return-timezone';
-
-// @injectable()
-// export class FindAppointmentsService {
-//   constructor(
-//     @inject('AppointmentsRepository')
-//     private appointmentsRepository: IAppointmentsRepository,
-
-//     @inject('ProfessionalsRepository')
-//     private professionalsRepository: IProfessionalsRepository,
-
-//     @inject('AddressesRepository')
-//     private addressesRepository: IAddressesRepository,
-//   ) {}
-
-//   public async execute(
-//     query: any,
-//   ): Promise<(Appointment & { timeZone?: string | null })[]> {
-//     const { patientId, professionalId, finished, appointmentId } = query;
-
-//     const where: FindOptionsWhere<Appointment> = {};
-
-//     if (patientId) where.patientId = patientId;
-//     if (appointmentId) where.id = appointmentId;
-
-//     if (professionalId) {
-//       if (professionalId === 'null') {
-//         where.professionalId = IsNull();
-//       } else if (professionalId === '!null') {
-//         where.professionalId = Not(IsNull());
-//       } else {
-//         where.professionalId = professionalId;
-//       }
-//     }
-
-//     if (typeof finished === 'boolean') {
-//       where.finished = finished;
-//     }
-
-//     const professional = await this.professionalsRepository.findById(
-//       professionalId,
-//     );
-
-//     const appointments = await this.appointmentsRepository.find(where);
-
-//     const patientsWithTimeZones = await Promise.all(
-//       appointments.map(async appointment => {
-//         const address = await this.addressesRepository.findByPatientId(
-//           appointment.patientId,
-//         );
-
-//         const timeZoneValidate = verifyTimeZone(
-//           address?.country,
-//           address?.state,
-//           address?.city,
-//         );
-
-//         if (!timeZoneValidate) {
-//           console.log(
-//             `Fuso não identificado ou dados incompletos. País ${address?.country} - Estado ${address?.state} - Cidade ${address?.city}`,
-//           );
-//         }
-
-//         return {
-//           ...appointment,
-//           timeZone: timeZoneValidate,
-//         };
-//       }),
-//     );
-
-//     return patientsWithTimeZones;
-//   }
-// }
-
-import moment from 'moment-timezone';
 import { FindOptionsWhere, IsNull, Not } from 'typeorm';
 import { injectable, inject } from 'tsyringe';
 
+import { IAddressesRepository } from '../../addresses/contracts/repositories/IAddressesRepository';
 import { IProfessionalsRepository } from '../../professionals/contracts/repositories/professionals';
 import { Appointment } from '../entities/appointment';
 import { IAppointmentsRepository } from '../contracts/repositories/appointments';
+import { verifyTimeZone } from '../utils/return-timezone';
 
 @injectable()
 export class FindAppointmentsService {
@@ -97,11 +15,14 @@ export class FindAppointmentsService {
 
     @inject('ProfessionalsRepository')
     private professionalsRepository: IProfessionalsRepository,
+
+    @inject('AddressesRepository')
+    private addressesRepository: IAddressesRepository,
   ) {}
 
   public async execute(
     query: any,
-  ): Promise<(Appointment & { convertedDate?: string })[]> {
+  ): Promise<(Appointment & { timeZone?: string | null })[]> {
     const { patientId, professionalId, finished, appointmentId } = query;
 
     const where: FindOptionsWhere<Appointment> = {};
@@ -125,42 +46,33 @@ export class FindAppointmentsService {
 
     const appointments = await this.appointmentsRepository.find(where);
 
-    const appointmentsWithConvertedDates = await Promise.all(
+    const patientsWithTimeZones = await Promise.all(
       appointments.map(async appointment => {
-        if (!appointment.professionalId) {
-          return {
-            ...appointment,
-            convertedDate: null,
-          };
-        }
-
-        const professional = await this.professionalsRepository.findById(
-          appointment.professionalId,
+        const address = await this.addressesRepository.findByPatientId(
+          appointment.patientId,
         );
 
-        const professionalTimeZone = professional?.professionalTimezone;
+        console.log(address);
 
-        if (!professionalTimeZone) {
+        const timeZoneValidate = verifyTimeZone(
+          address?.country,
+          address?.state,
+          address?.city,
+        );
+
+        if (!timeZoneValidate) {
           console.log(
-            `Fuso horário do profissional não encontrado para o profissional ${appointment.professionalId}`,
+            `Fuso não identificado ou dados incompletos. País ${address?.country} - Estado ${address?.state} - Cidade ${address?.city}`,
           );
-          return {
-            ...appointment,
-            convertedDate: null,
-          };
         }
-
-        const convertedDate = moment(appointment.date)
-          .tz(professionalTimeZone)
-          .format('YYYY-MM-DD HH:mm:ss');
 
         return {
           ...appointment,
-          convertedDate,
+          timeZone: timeZoneValidate,
         };
       }),
     );
 
-    return appointmentsWithConvertedDates;
+    return patientsWithTimeZones;
   }
 }
