68bbe801c2fa555f92e1a61695b0c609c983d816
Bruno Bastos <brunobbastosf@gmail.com>
2025-03-13 17:23:05 -0300

    feat: console.logs

--- src/shared/middlewares/personalizated-token.ts
+++ src/shared/middlewares/personalizated-token.ts
@@ -8,8 +8,11 @@ export const personalizatedAuthentication = (
 ): Response | void => {
   const authHeader = request.headers.authorization;
   const token = process.env.BMX_TOKEN;
+  console.log('authHeader', authHeader);
+  console.log('token', token);
 
   if (authHeader === token) {
+    console.log('São iguais');
     return next();
   }
   throw new AppError('Token inválido');
