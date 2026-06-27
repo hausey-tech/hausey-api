1a5a4112bcca7fab95e348907fee231d859d2e91
luiscadari <luiscadari@icloud.com>
2026-06-27 12:46:37 -0300

    fix: plan validation (isPro, expliresAt)

--- src/modules/plans/contracts/schemas/create-plan-schema.ts
+++ src/modules/plans/contracts/schemas/create-plan-schema.ts
@@ -8,12 +8,12 @@ export const CreatePlanSchema = {
       price: Joi.number().required(),
       type: Joi.string().valid('individual', 'family').default('individual'),
       maxDependents: Joi.number().integer().min(0).default(0),
+      isPro: Joi.boolean().default(false),
     })
     .when(Joi.object({ type: 'family' }).unknown(), {
       then: Joi.object({
         maxDependents: Joi.number().integer().min(1).required(),
       }),
-      isPro: Joi.boolean(),
     }),
   [Segments.QUERY]: Joi.object().keys({
     regions: Joi.string(),
--- src/shared/typeorm/migrations/1750000000000-AddIsProAndTypeToPlanAndPatient.ts
+++ src/shared/typeorm/migrations/1750000000000-AddIsProAndTypeToPlanAndPatient.ts
@@ -9,17 +9,17 @@ export class AddIsProAndTypeToPlanAndPatient1750000000000
     await queryRunner.query(
       `ALTER TABLE "plans" ADD "is_pro" boolean NOT NULL DEFAULT false`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "plans" ADD "type" character varying NULL`,
-    );
+    // await queryRunner.query(
+    //   `ALTER TABLE "plans" ADD "type" character varying NULL`,
+    // );
     await queryRunner.query(
       `ALTER TABLE "patients" ADD "is_pro" boolean NOT NULL DEFAULT false`,
     );
     await queryRunner.query(
-      `UPDATE "plans" SET "is_pro" = true, "type" = 'Individual' WHERE "id" = '938ef52c-bd6a-4e21-a6e5-e2eba8b5e60f'`,
+      `UPDATE "plans" SET "is_pro" = true, "type" = 'individual' WHERE "id" = '938ef52c-bd6a-4e21-a6e5-e2eba8b5e60f'`,
     );
     await queryRunner.query(
-      `UPDATE "plans" SET "is_pro" = true, "type" = 'Familiar' WHERE "name" ILIKE 'Familiar%'`,
+      `UPDATE "plans" SET "is_pro" = true, "type" = 'family' WHERE "name" ILIKE '%Familiar%'`,
     );
     await queryRunner.query(
       `UPDATE "patients" SET "is_pro" = true WHERE "plan_id" IN (SELECT "id" FROM "plans" WHERE "is_pro" = true)`,
@@ -28,7 +28,7 @@ export class AddIsProAndTypeToPlanAndPatient1750000000000
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "patients" DROP COLUMN "is_pro"`);
-    await queryRunner.query(`ALTER TABLE "plans" DROP COLUMN "type"`);
+    // await queryRunner.query(`ALTER TABLE "plans" DROP COLUMN "type"`);
     await queryRunner.query(`ALTER TABLE "plans" DROP COLUMN "is_pro"`);
   }
 }
